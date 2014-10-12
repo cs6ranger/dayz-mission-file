4b0747a5701a28804fcb2a0732faaa9962a87204
cs6ranger <ryan.spalding@gmail.com>
2014-10-12 02:24:45 -0400

    moving chainsaw to item vendor

---
+++ dayz_1.napf/mods/CfgServerTrader/Category/NeutralChainSaw.hpp
@@ -0,0 +1,25 @@
+class Category_699 {
+	class ChainSawB {
+		type = "trade_items";
+		buy[] = {1,"ItemGoldBar"};
+		sell[] = {5,"ItemGoldBar"};
+	};
+	class ItemMixOil {
+		type = "trade_items";
+		buy[] = {1,"ItemGoldBar"};
+		sell[] = {5,"ItemGoldBar"};
+	};
+};
+
+class Category_700 {
+	class ChainSaw {
+		type = "trade_items";
+		buy[] = {1,"ItemGoldBar"};
+		sell[] = {5,"ItemGoldBar"};
+	};
+	class ItemMixOil {
+		type = "trade_items";
+		buy[] = {1,"ItemGoldBar"};
+		sell[] = {5,"ItemGoldBar"};
+	};
+};
--- dayz_1.napf/mods/CfgServerTrader/cfgServerTrader.hpp
+++ dayz_1.napf/mods/CfgServerTrader/cfgServerTrader.hpp
@@ -83,4 +83,5 @@ class CfgTraderCategory {
 	#include "Category\NeutralVehicleParts.hpp"
 	#include "Category\NeutralWholesale.hpp"
 	#include "Category\Overpoch.hpp"
+	#include "Category\NeutralChainSaw.hpp"
 };
